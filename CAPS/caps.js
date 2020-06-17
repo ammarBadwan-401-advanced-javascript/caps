b1040296db63c87a13f1b73f6bdd0104afa10303
Ammar <ammar.badwan95@gmail.com>
2020-06-17 13:08:47 +0300

    finished lab 18

--- CAPS/caps.js
+++ CAPS/caps.js
@@ -7,89 +7,32 @@ const io = require('socket.io')(PORT);
 
 const caps = io.of('/caps');
 caps.on('connection',socket=>{
-  console.log('Welcome to CAPS, user-'+socket.id);
-  let currentRoom = '';
+  console.log('Connected: '+socket.id);
 
   socket.on('join', room=>{
-    socket.leave(currentRoom);
     socket.join(room);
-    currentRoom = room;
-    console.log('someonne joined the room' + room);
-    caps.to(currentRoom).emit('message');
-
-
-    socket.on('pickup', order=>{
-      io.emit(order);
-      let time = new Date();
-      console.log('EVENT',{event:order.event,time,payload: order.payload});
-    });
-  
-    socket.on('in-transit', payload=>{
-      caps.to('vendor').emit('in-transit',payload);
-      console.log('someone is here');
-
-    });
-  
-    socket.on('delivered',payload=>{
-      caps.to('vendor').emit('delivered',payload);
-    });
-
-
+    console.log('Joined the room: ' + room);
   });
 
-
-});
-
-
-
-
-
-/*
-const net = require('net');
-
-
-
-
-const server = net.createServer(); 
-server.listen(PORT, ()=> console.log(`Server is up on ${PORT}`));
-
-let socketPool = {};
-let iterator = 0;
-
-server.on('connection',(socket)=>{
-  const id =`Socket-${iterator++}`;
-
-  console.log(`Client with ID ${id} got connected!`);
-  socketPool[id] = socket;
-
-  socket.on('end', (end)=>{
-    console.log('connection ended', end);
-    delete socketPool[id];
-  });
-
-  socket.on('data',(data)=>dataParser(data));
-});
-
-server.on('error', (e)=> {
-  console.log('SERVER ERROR', e);
-});
-
-
-function dataParser(data){
-  let order = JSON.parse(data.toString());
-  if(order.event && order.payload){
+  socket.on('pickup', order=>{
+    io.emit(order);
     let time = new Date();
-    console.log('EVENT',{event:order.event,time,payload: order.payload});
-    broadcast(order);
-  } else {
-    console.log('Non-legitimate object recieved');
-  }
-}
+    order.time = time;
+    console.log('EVENT',order);
+    caps.emit('pickup',order);
+  });
 
-function broadcast(data){
-  let payload = JSON.stringify(data);
-  for (let socket in socketPool){
-    socketPool[socket].write(payload);
-  }
-}
-*/
+  socket.on('in-transit', order=>{
+    let time = new Date();
+    order.time = time;
+    console.log('EVENT',order);
+    caps.to(order.storeName).emit('in-transit',order);
+  });
+
+  socket.on('delivered',order=>{
+    let time = new Date();
+    order.time = time;
+    console.log('EVENT',order);
+    caps.to(order.storeName).emit('delivered',order);
+  });
+});
--- vendor/vendor.js
+++ vendor/vendor.js
@@ -1,40 +1,24 @@
 'use strict';
 require('dotenv').config();
-const net = require('net');
 const faker = require('faker');
 const STORE_NAME = process.env.STORE_NAME || 'Ammar Store';
 
-const client = new net.Socket();
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000/caps');
 
-client.connect(PORT, HOST, ()=> {console.log(`${STORE_NAME} got connected on ${HOST}:${PORT}`)});
+socket.emit('join',STORE_NAME);
 
-const orders = [];
-
-client.on('data',function(data){
-  let recievedObj = JSON.parse(data);
-  if (recievedObj.event === 'delivered'){
-    console.log(`Thank you for delivering ${recievedObj.payload.orderId}`);
-  }
+socket.on('delivered', payload=>{
+  console.log(`Thank you for delivering ${payload.orderId}`)
 })
 
+
 setInterval(()=>{
-    makeOrder();
+  let order = {
+    storeName: STORE_NAME,
+    orderId: faker.random.uuid(),
+    customerName: faker.name.firstName(),
+    address: faker.address.streetAddress(),
+  };
+  socket.emit('pickup', order)
 },5000);
-
-
-function makeOrder(){
-    let order = {
-        storeName: STORE_NAME,
-        orderId: faker.random.uuid(),
-        customerName: faker.name.firstName(),
-        address: faker.address.streetAddress(),
-      };
-    let event = JSON.stringify({event:'pickup', payload: order})
-    client.write(event);
-}
-
-client.on('error',(e)=>{
-  console.log('Error recieved: ' + e);
-});
